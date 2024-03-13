a12cddde14f0e08e39cd97029db2ba869bc17c97
Jackson Dev <jackson.faria.develop@gmail.com>
2024-03-13 20:18:29 -0300

    bg color

--- uServ/_api/serializers/category/categories_serializer.py
+++ uServ/_api/serializers/category/categories_serializer.py
@@ -4,4 +4,4 @@ from _panel.models import Category
 class CategoriesSerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'name', 'parent', 'active', 'children', 'text_color', 'background_color')
+        fields = ('id', 'name', 'parent', 'active', 'children', 'text_color', 'bg_color')
